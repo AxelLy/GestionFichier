243c212f0b4c9723bbaf2abcc7416aef59898f36
Dim31 <46557331+Dim31@users.noreply.github.com>
2019-02-04 14:36:05 +0100

    ye

--- gfserveur.js
+++ gfserveur.js
@@ -2,19 +2,25 @@
 var http = require("http");
 var fs = require('fs');
 var vm = require('vm');
+var express = require('express');
+var app = express();
 var controleur = require("./gfcontroleur");
 
 
 
 
-//var app = require('express');
-
+var myObjJson = controleur.Controller.gestiondossiers();
+app.get('/accueil', function (req, res) {
+  res.send("wsh")
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
 // Crée un serveur HTTP qui écoute les requêtes sur le port 8000
 http.createServer(function(request, response) {
-
  
   // Envoie le corps de la réponse "Hello World"
-  response.end('ok\n');
+  response.end('ok');
 }).listen(8000);
 
 // app.get('/trc', ifiofiyfifiyf)
@@ -22,6 +28,6 @@ http.createServer(function(request, response) {
 // Édite l'URL pour accéder au serveur
 console.log('Server running at http://127.0.0.1:8000/');
 
-controleur.Controller.gestiondossiers();
 
-module.exports = { fs}
+
+module.exports = {fs}
