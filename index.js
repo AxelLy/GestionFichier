82ed468e52ad565ae53ac0f991d23784ab3a4b6a
Dim31 <46557331+Dim31@users.noreply.github.com>
2019-02-13 14:39:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var m = require("mithril")
-var list = require("C:/Users/Dimi/Documents/GitHub/GestionFichier/myapp/src/list.js")
-list.loadList()
+import m from "mithril"
+import List from "C:/Users/Dimi/Documents/GitHub/GestionFichier/myapp/src/list.js"
+import Chart from "C:/Users/Dimi/Documents/GitHub/GestionFichier/myapp/src/chart.js"
+List.loadList()
+Chart.loadList()
+m.mount(document.body,List)
